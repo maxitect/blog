bc07056defc29f019594d940a43d6af8b9e1535e
maxitect <maxime.downe@gmail.com>
2025-04-19 21:40:58 +0100

    feat: add image in PostItem.tsx component

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { Post } from "../lib/posts";
 import Date from "./Date";
 
@@ -10,25 +11,31 @@ interface PostItemProps {
 
 export default function PostItem({ post }: PostItemProps) {
   return (
-    <article className="mb-6 p-5 border-2 border-foreground">
-      <h3 className="text-2xl font-bold mb-2 mt-0 !pt-0 font-condensed-medium">
+    <article className="flex flex-wrap gap-6 mb-6 p-5 border-2 border-foreground md:w-auto w-80">
+      {post.imagePath && (
+      <div className="flex-1 min-w-64">
+        <Image src={post.imagePath} alt={""} width="275" height="200" />
+      </div>)}
+      <div className="flex-1 min-w-64">
+        <h3 className="text-2xl font-bold mb-2 mt-0 !pt-0 font-condensed-medium">
+          <Link
+            href={`/posts/${post.slug}`}
+            className="hover:text-blue-600 hover:underline"
+          >
+            {post.title}
+          </Link>
+        </h3>
+        <Date date={post.date} />
+        <p className="text-base !text-foreground-mid mb-4 line-clamp-3">
+          {post.preview}
+        </p>
         <Link
           href={`/posts/${post.slug}`}
-          className="hover:text-blue-600 hover:underline"
+          className="inline-block border-2 border-foreground hover:bg-blue-600 py-2 px-4 transition-colors"
         >
-          {post.title}
+          Read more
         </Link>
-      </h3>
-      <Date date={post.date} />
-      <p className="text-base !text-foreground-mid mb-4 line-clamp-3">
-        {post.preview}
-      </p>
-      <Link
-        href={`/posts/${post.slug}`}
-        className="inline-block border-2 border-foreground hover:bg-blue-600 py-2 px-4 transition-colors"
-      >
-        Read more
-      </Link>
+      </div>
     </article>
   );
 }
