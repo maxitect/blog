627b4e9fb8e3b7eb97d37a4ec768b37ab52aae30
maxitect <maxime.downe@gmail.com>
2025-05-04 15:02:00 +0100

    style: update PostContent.tsx to style tables using remark-gsm

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import CodeBlock from "./Codeblock";
+import remarkGfm from "remark-gfm";
 
 interface PostContentProps {
   content: string;
@@ -9,7 +10,12 @@ interface PostContentProps {
 const PostContent: React.FC<PostContentProps> = ({ content }) => {
   return (
     <div className="markdown">
-      <ReactMarkdown components={{ code: CodeBlock }}>{content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{ code: CodeBlock }}
+        remarkPlugins={[remarkGfm]}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 };
